52493017e58b4c2411762bc814d2ae4cee7e7176
Rico Suter <mail@rsuter.com>
2018-08-23 15:26:18 +0200

    Set MSBUILD_EXE_PATH, closes #5

--- src/Dnt/Program.cs
+++ src/Dnt/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using Dnt.Commands;
 using Dnt.Commands.Infrastructure;
 using NConsole;
@@ -22,6 +24,8 @@ namespace Dnt
             ConsoleUtilities.Write("Binary: " + assembly.Location + "\n\n");
             try
             {
+                SetMsBuildExePath();
+
                 var processor = new CommandLineProcessor(new CoreConsoleHost());
                 processor.RegisterCommandsFromAssembly(typeof(CommandBase).Assembly);
                 processor.Process(args);
@@ -34,6 +38,30 @@ namespace Dnt
 
             if (Debugger.IsAttached)
                 Console.ReadLine();
+        }
+
+        private static void SetMsBuildExePath()
+        {
+#if NETCOREAPP
+            try
+            {
+                // See https://github.com/Microsoft/msbuild/issues/2532#issuecomment-381096259
+
+                var process = Process.Start(new ProcessStartInfo("dotnet", "--list-sdks") { RedirectStandardOutput = true });
+                process.WaitForExit(1000);
+
+                var output = process.StandardOutput.ReadToEnd();
+                var sdkPaths = Regex.Matches(output, "([0-9]+.[0-9]+.[0-9]+) \\[(.*)\\]").OfType<Match>()
+                    .Select(m => System.IO.Path.Combine(m.Groups[2].Value, m.Groups[1].Value, "MSBuild.dll"));
+
+                var sdkPath = sdkPaths.Last();
+                Environment.SetEnvironmentVariable("MSBUILD_EXE_PATH", sdkPath);
+            }
+            catch (Exception exception)
+            {
+                ConsoleUtilities.Write("Could not set MSBUILD_EXE_PATH: " + exception + "\n\n");
+            }
+#endif
         }
     }
 }
