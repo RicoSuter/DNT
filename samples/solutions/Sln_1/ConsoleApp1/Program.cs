e5e9042af0a3f8a5b85d09faf910e3da06e8667d
giboX <43074372+giboX@users.noreply.github.com>
2021-02-21 01:48:25 +0100

    add some output

--- samples/solutions/Sln_1/ConsoleApp1/Program.cs
+++ samples/solutions/Sln_1/ConsoleApp1/Program.cs
@@ -10,7 +10,9 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World from ConsoleApp1!");
 
-  
+            var cl10 = new ClassLibrary10.Class10_1();
+            cl10.Hello();
+
             Console.WriteLine("Press <Enter> to continue");
             Console.ReadLine();
         }
