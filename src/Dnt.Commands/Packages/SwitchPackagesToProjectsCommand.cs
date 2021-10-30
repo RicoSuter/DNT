7fd915aae9f3fa18bdb5a8d4d286902f1f7469f1
Heiki Vaher <heiki.vaher@jotautomation.com>
2021-10-31 00:18:24 +0300

    Added possibility to put projects into solution folder with configuration parameter 'solutionFolder' (#18)

--- src/Dnt.Commands/Packages/SwitchPackagesToProjectsCommand.cs
+++ src/Dnt.Commands/Packages/SwitchPackagesToProjectsCommand.cs
@@ -37,6 +37,7 @@ namespace Dnt.Commands.Packages
         {
             var solution = SolutionFile.Parse(configuration.ActualSolution);
             var projects = new List<string>();
+            var solutionFolderArg = "";
             foreach (var mapping in configuration.Mappings)
             {
                 foreach (var path in mapping.Value)
@@ -48,10 +49,12 @@ namespace Dnt.Commands.Packages
                 }
             }
 
+            if (!string.IsNullOrWhiteSpace(configuration.SolutionFolder))
+                solutionFolderArg = $" --solution-folder {configuration.SolutionFolder}";
             if (projects.Any())
             {
                 await ExecuteCommandAsync(
-                    "dotnet", "sln \"" + configuration.ActualSolution + "\" add " + string.Join(" ", projects), host);
+                    "dotnet", "sln \"" + configuration.ActualSolution + "\" add " + string.Join(" ", projects)+ solutionFolderArg, host);
             }
         }
 
